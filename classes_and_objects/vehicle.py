941f8fd284dfcedcf1ea4d331256d25a1268a769
Nowy <nowotny1993@gmail.com>
2021-09-19 14:32:31 +0300

    classes_and_objects_fundamentals

---
+++ classes_and_objects/vehicle.py
@@ -0,0 +1,40 @@
+class Vehicle:
+
+    def __init__(self, type, model, price, owner=None):
+        self.type = type
+        self.model = model
+        self.price = price
+        self.owner = owner
+
+    def buy(self, money, new_owner):
+        self.money = money
+        self.new_owner = new_owner
+        if self.owner != None:
+            return 'Car already sold'
+        else:
+            if self.money >= self.price:
+                self.owner = self.new_owner
+                return f"Successfully bought a {self.type}. Change: {(self.money - self.price):.2f}"
+            else:
+                return 'Sorry, not enough money'
+
+    def sell(self):
+        if self.owner != None:
+            self.owner = None
+        else:
+            return "Vehicle has no owner"
+    def __repr__(self):
+        if self.owner != None:
+            return f'{self.model} {self.type} is owned by: {self.owner}'
+        else:
+            return f'{self.model} {self.type} is on sale: {self.price}'
+
+vehicle_type = "car"
+model = "BMW"
+price = 30000
+vehicle = Vehicle(vehicle_type, model, price)
+print(vehicle.buy(15000, "Peter"))
+print(vehicle.buy(35000, "George"))
+print(vehicle)
+vehicle.sell()
+print(vehicle)
